c37ed7202543c8242353d753de5e8e9963f5a98a
professor-moody <professor.moody@pm.me>
2025-08-15 11:19:18 -0700

    CrowdStrike/sccmhound@35709e57d2e2eda5f69ccc452c7cce9aa6a762e8

---
+++ repos/sccmhound/src/models/UserMachineRelationship.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCCMHound.src.models
+{
+    public class UserMachineRelationship
+    {
+        public string resourceName { get; set; }
+        public string uniqueUserName { get; set; }
+
+        public UserMachineRelationship() { }
+
+        public UserMachineRelationship(string resourceName, string uniqueUserName)
+        {
+            this.resourceName = resourceName;
+            this.uniqueUserName = uniqueUserName;
+        }
+    }
+}
