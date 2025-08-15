4db3faabefee3a1cc74a6f5be79b8ae04a905488
professor-moody <professor.moody@pm.me>
2025-08-14 18:26:26 -0700

    7524d8ac767daf925c7fd617d2589da83144afdb

--- repos/sccmhound/SECURITY.md
+++
@@ -1,1 +0,0 @@
- oss-security@crowdstrike.com
--- repos/sccmhound/SharpHoundCommon/src/CommonLib/OutputTypes/SessionAPIResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SharpHoundCommonLib.OutputTypes
-{
-    public class SessionAPIResult : APIResult
-    {
-        public Session[] Results { get; set; } = Array.Empty<Session>();
-    }
-}
