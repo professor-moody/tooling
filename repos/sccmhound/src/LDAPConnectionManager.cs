a774330b03be84d2b5fe046648b83c4e106e72c1
professor-moody <professor.moody@pm.me>
2025-08-15 16:40:18 -0700

    CrowdStrike/sccmhound@35709e57d2e2eda5f69ccc452c7cce9aa6a762e8

---
+++ repos/sccmhound/src/LDAPConnectionManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Security.Principal;
+
+namespace SCCMHound.src
+{
+    public class LdapConnectionManager
+    {
+        private static LdapConnectionManager connectionManager;
+        private Dictionary<string, PrincipalContext> ldapPrincipals;
+        private List<string> failedDomains = [];
+
+        // Mutex for thread safety
+        private readonly object _lock = new object();
+
+        // Singleton
+        public static LdapConnectionManager Instance
+        {
+            get
+            {
+                if (connectionManager == null)
+                {
+                    connectionManager = new LdapConnectionManager();
+                }
+                return connectionManager;
+            }
+        }
+
+        // Private constructor
+        private LdapConnectionManager()
+        {
+            ldapPrincipals = new Dictionary<string, PrincipalContext>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        public PrincipalContext GetDomainContext(string domain)
+        {
+            if (string.IsNullOrEmpty(domain))
+                throw new ArgumentException("Domain cannot be null or empty");
+
+            lock (_lock)
+            {
+                // If the domain resolution previously failed, return null
+                if (this.failedDomains.Contains(domain)) {
+                    return null;
+                }
+
+                // Create a new domain context if it does not already exist
+                if (!ldapPrincipals.TryGetValue(domain, out PrincipalContext context))
+                {
+                    try
+                    {
+                        context = new PrincipalContext(ContextType.Domain, domain);
+                        ldapPrincipals[domain] = context;
+                        Console.WriteLine($"Created new PrincipalContext for domain: {domain}");
+                    }
+                    // If an exception is hit when trying to create a new principal context, add it to the list of failed domains and return null
+                    catch (Exception e)
+                    {
+                        this.failedDomains.Add(domain);
+                        return null;
+                    }
+                }
+                return context;
+            }
+        }
+
+        // Disposes all principal contexts
+        public void Cleanup()
+        {
+            lock (_lock)
+            {
+                foreach (var context in ldapPrincipals.Values)
+                {
+                    context.Dispose();
+                }
+                ldapPrincipals.Clear();
+            }
+        }
+    }
+}
---
+++ repos/sccmhound/src/models/ComputerLocalAdmins.cs
@@ -0,0 +1,78 @@
+﻿using SharpHoundCommonLib;
+using SharpHoundCommonLib.OutputTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCCMHound.src.models
+{
+    public class ComputerLocalAdmins
+    {
+        public ComputerExt computer { get; set; }
+        public List<User> users { get; set; } = new List<User>();
+        public List<Group> groups { get; set; } = new List<Group>();
+
+        public ComputerLocalAdmins(ComputerExt computer)
+        {
+            this.computer = computer;
+        }
+
+        public void AddAdminUser(User user)
+        {
+            this.users.Add(user);
+        }
+
+        public void AddAdminGroup(Group group)
+        {
+            this.groups.Add(group);
+        }
+
+        public void PopulateComputerExtWithLocalAdminData(string groupSID)
+        {
+
+            LocalGroupAPIResult admins = new LocalGroupAPIResult
+            {
+                ObjectIdentifier = groupSID,
+                Name = $"ADMINISTRATORS@{this.computer.Properties["name"]}",
+
+            };
+            List<TypedPrincipal> tempList = new List<TypedPrincipal>();
+            List<SHLocalAdmin> tempLocalAdmins = new List<SHLocalAdmin>();
+
+
+            foreach (User user in users)
+            {
+                var res = new TypedPrincipal
+                (
+                    user.ObjectIdentifier,
+                    SharpHoundCommonLib.Enums.Label.User
+                   
+                );
+
+                tempList.Add(res);
+                tempLocalAdmins.Add(new SHLocalAdmin(user.ObjectIdentifier, "User"));
+            }
+
+            foreach (Group group in groups)
+            {
+                var res = new TypedPrincipal
+                (
+                    group.ObjectIdentifier,
+                    SharpHoundCommonLib.Enums.Label.Group
+
+                );
+                tempList.Add(res);
+                tempLocalAdmins.Add(new SHLocalAdmin(group.ObjectIdentifier, "Group"));
+
+
+            }
+            admins.Results = tempList.ToArray();
+            admins.Collected = true;
+            this.computer.LocalAdmins = new SHLocalAdmins(tempLocalAdmins.ToArray());
+
+        }
+
+    }
+}
