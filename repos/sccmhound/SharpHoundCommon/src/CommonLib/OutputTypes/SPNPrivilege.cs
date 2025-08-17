6b5aae6ae4a137c7ade25ecb585b05ae3fd39933
professor-moody <professor.moody@pm.me>
2025-08-17 14:00:37 -0700

    410254bbd0ed10329db05b64ae667516204936b1

--- repos/sccmhound/SharpHoundCommon/src/CommonLib/OutputTypes/SPNPrivilege.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SharpHoundCommonLib.OutputTypes
-{
-    public class SPNPrivilege
-    {
-        public string ComputerSID { get; set; }
-        public int Port { get; set; }
-        public string Service { get; set; }
-    }
-}
