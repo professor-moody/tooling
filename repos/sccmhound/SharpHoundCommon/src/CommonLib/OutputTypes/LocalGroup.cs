2e49795a71dbe691937b9e568576f439bdb177b1
professor-moody <professor.moody@pm.me>
2025-08-17 13:22:06 -0700

    f58173fd0b6d88b8dbf131a9053752572aee53c4

--- repos/sccmhound/SharpHoundCommon/src/CommonLib/OutputTypes/LocalGroup.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SharpHoundCommonLib.OutputTypes
-{
-    public class LocalGroup
-    {
-        public string Name { get; set; }
-        public int Rid { get; set; }
-        public string ObjectID { get; set; }
-    }
-}
