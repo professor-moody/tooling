5f749e37d19b480aa01e7179bfed0de1970d9c42
professor-moody <professor.moody@pm.me>
2025-08-16 19:23:52 -0700

    CrowdStrike/sccmhound@35709e57d2e2eda5f69ccc452c7cce9aa6a762e8

---
+++ repos/sccmhound/src/UsersGroupsResolver.cs
@@ -0,0 +1,43 @@
+﻿using SCCMHound.src.models;
+using SharpHoundCommonLib.OutputTypes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Group = SharpHoundCommonLib.OutputTypes.Group;
+
+namespace SCCMHound.src
+{
+    public class UsersGroupsResolver
+    {
+        public UsersGroupsResolver(List<User> users, List<Group> groups)
+        {
+            Dictionary<string, Group> groupLookupByGroupName = HelperUtilities.createLookupTableGroups(groups);
+
+            foreach (User user in users)
+            {
+                string[] sccmUserGroupNames = (string[])user.Properties["sccmUserGroupName"];
+
+                foreach (string sccmUserGroupName in sccmUserGroupNames)
+                {
+                    string groupName = $"{sccmUserGroupName.Split('\\')[1].ToUpper()}@{user.Properties["domain"]}".ToLower();
+                    if (groupLookupByGroupName.ContainsKey(groupName))
+                    {
+                        Group group = groupLookupByGroupName[groupName];
+                        TypedPrincipal principal = new TypedPrincipal(user.ObjectIdentifier, SharpHoundCommonLib.Enums.Label.User);
+
+                        List<TypedPrincipal> tempList = group.Members.ToList();
+                        tempList.Add(principal);
+                        group.Members = tempList.ToArray();
+                    }
+                }
+                user.Properties.Remove("sccmUserGroupName");
+            }
+
+        }
+    }
+}
