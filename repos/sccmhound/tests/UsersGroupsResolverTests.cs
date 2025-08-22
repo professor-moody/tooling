96d731602a01eb9e70577f8d18c9e33b6bb8a3e9
professor-moody <professor.moody@pm.me>
2025-08-21 22:34:02 -0500

    CrowdStrike/sccmhound@35709e57d2e2eda5f69ccc452c7cce9aa6a762e8

---
+++ repos/sccmhound/tests/UsersGroupsResolverTests.cs
@@ -0,0 +1,25 @@
+﻿using SCCMHound.src.models;
+using SCCMHound.src;
+using SharpHoundCommonLib.OutputTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SCCMHound.tests
+{
+    public class UsersGroupsResolverTests
+    {
+        [Fact]
+        public void UsersGroupsResolver_test()
+        {
+            List<User> users = SCCMCollectorTests.DeserializeUsers();
+            List<Group> groups = SCCMCollectorTests.DeserializeGroups();
+            UsersGroupsResolver usersGroupsResolver = new UsersGroupsResolver(users, groups);
+
+            Assert.True(true);
+        }
+    }
+}
