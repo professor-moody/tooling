b53abfef623971d79d600de20086f48f1d42f719
professor-moody <professor.moody@pm.me>
2025-10-23 22:38:42 -0500

    CrowdStrike/sccmhound@35709e57d2e2eda5f69ccc452c7cce9aa6a762e8

---
+++ repos/sccmhound/SharpHoundCommon/test/unit/Facades/SAMMocks/WorkstationMocks/MockWorkstationAliasTestGroup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Principal;
+using SharpHoundRPC;
+using SharpHoundRPC.Wrappers;
+
+namespace CommonLibTest.Facades
+{
+    [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
+    public class MockWorkstationAliasTestGroup : ISAMAlias
+    {
+        public Result<IEnumerable<SecurityIdentifier>> GetMembers()
+        {
+            return new List<SecurityIdentifier>();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
