efeb4b4cbdc12b323acaaa671dc60ae32924d29b
professor-moody <professor.moody@pm.me>
2025-12-04 23:57:24 -0600

    CrowdStrike/sccmhound@35709e57d2e2eda5f69ccc452c7cce9aa6a762e8

---
+++ repos/sccmhound/SharpHoundCommon/src/CommonLib/OutputTypes/MetaTag.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace SharpHoundCommonLib.OutputTypes
+{
+    [DataContract]
+    public class MetaTag
+    {
+        [DataMember(Name = "methods")] public long CollectionMethods { get; set; }
+        [DataMember(Name = "type")] public string DataType { get; set; }
+        [DataMember(Name = "count")] public long Count { get; set; }
+        [DataMember(Name = "version")] public int Version { get; set; }
+        [DataMember(Name = "collectorversion")] public string CollectorVersion { get; set; }
+    }
+}
