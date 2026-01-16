a157e03d5825b03f542efba89bd68844d447676f
professor-moody <professor.moody@pm.me>
2026-01-15 22:38:22 -0600

    CrowdStrike/sccmhound@35709e57d2e2eda5f69ccc452c7cce9aa6a762e8

---
+++ repos/sccmhound/SharpHoundCommon/src/CommonLib/OutputTypes/OU.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharpHoundCommonLib.OutputTypes
+{
+    public class OU : OutputBase
+    {
+        public ResultingGPOChanges GPOChanges = new();
+        public GPLink[] Links { get; set; } = Array.Empty<GPLink>();
+        public TypedPrincipal[] ChildObjects { get; set; } = Array.Empty<TypedPrincipal>();
+        public string[] InheritanceHashes { get; set; } = Array.Empty<string>();
+    }
+}
