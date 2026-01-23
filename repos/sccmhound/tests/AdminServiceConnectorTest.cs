4fc0c3c08928b76663758cf5f6f09d588c951b66
professor-moody <professor.moody@pm.me>
2026-01-22 22:36:54 -0600

    CrowdStrike/sccmhound@35709e57d2e2eda5f69ccc452c7cce9aa6a762e8

---
+++ repos/sccmhound/tests/AdminServiceConnectorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SCCMHound.tests
+{
+    public class AdminServiceConnectorTests
+    {
+        [Fact]
+        public void connect_success()
+        {
+            AdminServiceConnector adminServiceConnector = AdminServiceConnector.CreateInstance(TestConstants.testInstanceIP, null);
+            Assert.NotNull(adminServiceConnector);
+        }
+
+        [Fact]
+        public void connect_failure()
+        {
+            AdminServiceConnector adminServiceConnector = AdminServiceConnector.CreateInstance(TestConstants.testUnroutableIP, null);
+            Assert.Null(adminServiceConnector);
+
+        }
+    }
+}
