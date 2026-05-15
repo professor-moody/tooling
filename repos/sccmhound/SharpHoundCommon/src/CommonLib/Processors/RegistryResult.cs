ad2889c5def48db16b42031359668612190afdf8
professor-moody <professor.moody@pm.me>
2026-05-15 01:10:02 -0500

    CrowdStrike/sccmhound@35709e57d2e2eda5f69ccc452c7cce9aa6a762e8

---
+++ repos/sccmhound/SharpHoundCommon/src/CommonLib/Processors/RegistryResult.cs
@@ -0,0 +1,7 @@
+using SharpHoundCommonLib.OutputTypes;
+
+namespace SharpHoundCommonLib.Processors {
+    public class RegistryResult : APIResult {
+        public object Value { get; set; }
+    }
+}
