3cfd4f254b0f4c551c3e79eebb30cc8558e86200
professor-moody <professor.moody@pm.me>
2026-05-22 01:09:09 -0500

    CrowdStrike/sccmhound@35709e57d2e2eda5f69ccc452c7cce9aa6a762e8

---
+++ repos/sccmhound/SharpHoundCommon/test/unit/Facades/MockableForest.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.DirectoryServices.ActiveDirectory;
+
+namespace CommonLibTest.Facades
+{
+    [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
+    public class MockableForest
+    {
+        public static Forest Construct(string forestDnsName)
+        {
+            var forest = FacadeHelpers.GetUninitializedObject<Forest>();
+            FacadeHelpers.SetField(forest, "_forestDnsName", forestDnsName);
+
+            return forest;
+        }
+    }
+}
