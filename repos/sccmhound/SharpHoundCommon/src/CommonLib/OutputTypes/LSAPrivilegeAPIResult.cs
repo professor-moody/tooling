00cca1f4e6a2893a90f16358d81f1f338d48c72f
professor-moody <professor.moody@pm.me>
2026-07-16 22:11:34 -0500

    5f83d386696ba9521f358f80510e021681be8731

--- repos/sccmhound/SharpHoundCommon/src/CommonLib/OutputTypes/LSAPrivilegeAPIResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SharpHoundCommonLib.OutputTypes
-{
-    public class LSAPrivilegeAPIResult : APIResult
-    {
-        public TypedPrincipal[] Results { get; set; } = Array.Empty<TypedPrincipal>();
-    }
-}
