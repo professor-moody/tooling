a0beb076001d295f539bccad6e8cd817242cd88d
professor-moody <professor.moody@pm.me>
2026-08-14 00:57:25 -0500

    CrowdStrike/sccmhound@35709e57d2e2eda5f69ccc452c7cce9aa6a762e8

---
+++ repos/sccmhound/SharpHoundCommon/src/CommonLib/NativeMethods.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
+using SharpHoundRPC.NetAPINative;
+
+namespace SharpHoundCommonLib
+{
+    [ExcludeFromCodeCoverage]
+    // This class exists entirely as a shim for testing
+    public class NativeMethods
+    {
+        private readonly ILogger _log;
+
+        public NativeMethods(ILogger log = null)
+        {
+            _log = log ?? Logging.LogProvider.CreateLogger("NativeMethods");
+        }
+
+        public NativeMethods()
+        {
+            _log = Logging.LogProvider.CreateLogger("NativeMethods");
+        }
+
+        public virtual NetAPIResult<IEnumerable<NetSessionEnumResults>> NetSessionEnum(string serverName)
+        {
+            return NetAPIMethods.NetSessionEnum(serverName);
+        }
+
+        public virtual NetAPIResult<IEnumerable<NetWkstaUserEnumResults>> NetWkstaUserEnum(string servername)
+        {
+            return NetAPIMethods.NetWkstaUserEnum(servername);
+        }
+
+        public virtual NetAPIResult<NetAPIStructs.DomainControllerInfo> CallDsGetDcName(string computerName,
+            string domainName, uint flags)
+        {
+            return NetAPIMethods.DsGetDcName(computerName, domainName, flags);
+        }
+
+        public virtual NetAPIResult<NetAPIStructs.WorkstationInfo100> CallNetWkstaGetInfo(string serverName) {
+            return NetAPIMethods.NetWkstaGetInfo(serverName);
+        }
+    }
+}
